d42db52fbd8b96f487ac2a171fdd436f510e0f64
MattSamster <56278520+MattSamster@users.noreply.github.com>
2023-09-08 10:43:25 -0700

    initial commit for accelstepper library

--- Hardware/Arm-Firmware/main/main.ino
+++ Hardware/Arm-Firmware/main/main.ino
@@ -1,3 +1,6 @@
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
 #include <Arduino.h>
 #include "Stepper.h"
 #include <ros.h>
@@ -35,28 +38,17 @@ ros::Subscriber<ob1_arm_hw_interface::armCmd> s("/arduino/armCmd", &armCmdCb);
 ros::Publisher p("/arduino/armState", &armState);
 
 // bicep 
-#define pulse_1 3
-#define dir_1 4
+#define stepPin_1 3
+#define dirPin_1 4
 // bicep 
-#define pulse_2 5
-#define dir_2 6
+#define stepPin_2 5
+#define dirPin_2 6
 // shoulder 
-#define pulse_3 7
-#define dir_3 8
+#define stepPin_3 7
+#define dirPin_3 8
 
-// enable all steppers
-#define enable 13
 // steps per rev:
 #define STEPSPERREV 4000
-int PULpin[3] = {pulse_1, pulse_2, pulse_3};
-int DIRpin[3] = {dir_1, dir_2, dir_3};
-
-// make 3 stepper objects
-// Stepper.Stepper(int _enable, int _pulse, int _dir, int _steps) 
-
-Stepper mySteppers[3] = {Stepper(enable, PULpin[0], DIRpin[0], STEPSPERREV),
-                         Stepper(enable, PULpin[1], DIRpin[1], STEPSPERREV),
-                         Stepper(enable, PULpin[2], DIRpin[2], STEPSPERREV)};
 
 void setup() {
   // set up serial jic we need to monitor anything
@@ -69,22 +61,27 @@ void setup() {
   nh.advertise(p);
   nh.subscribe(s);
 
+  // instantiate steppers
+  AccelStepper stepper_1(AccelStepper::DRIVER, stepPin_1, dirPin_1);
+  AccelStepper stepper_2(AccelStepper::DRIVER, stepPin_2, dirPin_2);
+  AccelStepper stepper_3(AccelStepper::DRIVER, stepPin_3, dirPin_3);
+
+  // set parameters for the steppers
+  stepper1.setMaxSpeed(200.0);
+  stepper1.setAcceleration(100.0);
+
+  stepper2.setMaxSpeed(200.0);
+  stepper2.setAcceleration(100.0);
+
+  stepper3.setMaxSpeed(200.0);
+  stepper3.setAcceleration(100.0);
 }
 
 void loop() {
 
-    /*
-      TODO: 
-        * set up encoders --> they will be posn_actual's
-        * serial input --> input a nx6 array of joint angles (degrees)
-        *   currently this is produced by the input_sim.py script
-        *   one at a time (they are all one degree at a time)
-    */
-
-
   p.publish( &armState );
   nh.spinOnce();
-  delay(10);
+  delay(10); // purpose of delay?
   
   for(int i = 0; i < num_joints; i++){
     armState.angle[i] = 0.3;
@@ -98,48 +95,9 @@ void loop() {
     position_goal[i] =  armState.angle[i]
   }
 
-/*
-Write serial to send current joint positions
-*/
+// move joints based on position_goal
+steppers.moveTo(position_goal);
+steppers.setSpeed(50); //might need to set manually
+steppers.runSpeedToPosition(); // Blocks until all are in position
 
-
-    if(position_goal[0] != position_measured[0]){
-      //if position is not where you want it to be, step one time
-      if((position_goal[0] - position_measured[0]) > 0){
-        mySteppers[0].stepCW(1);
-      }
-      if((position_goal[0] - position_measured[0]) < 0){
-        mySteppers[0].stepCCW(1);
-      }
-      if((position_goal[0] - position_measured[0]) == 0){
-        Serial.println("motor_1 is in desired posn");
-      }
-    }
-    if(position_goal[1] != position_measured[1]){
-      //if position is not where you want it to be, step one time
-      if((position_goal[1] - position_measured[1]) > 0){
-        mySteppers[1].stepCW(1);
-      }
-      if((position_goal[1] - position_measured[1]) < 0){
-        mySteppers[1].stepCCW(1);
-      }
-      if((position_goal[1] - position_measured[1]) == 0){
-        Serial.println("motor_2 is in desired posn");
-      }
-    }
-    
-    if(position_goal[2] != position_measured[2]){
-      //if position is not where you want it to be, step one time
-      if((position_goal[2] - position_measured[2]) > 0){
-        mySteppers[2].stepCW(1);
-      }
-      if((position_goal[2] - position_measured[2]) < 0){
-        mySteppers[2].stepCCW(1);
-      }
-      if((position_goal[2] - position_measured[2]) == 0){
-        Serial.println("motor_3 is in desired posn");
-      }
-    }
-
-// send new joint positions
 }
